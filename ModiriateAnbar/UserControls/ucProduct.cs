ca6f5c4546b2f0de7768567af9e9435a4ddd7f3e
chegeni-0608 <mmoa80610@gmail.com>
2024-08-03 12:09:38 +0330

    2024-08-03appconfig

--- ModiriateAnbar/UserControls/ucProduct.cs
+++ ModiriateAnbar/UserControls/ucProduct.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
@@ -30,12 +31,40 @@ namespace ModiriateAnbar.UserControls
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-
+            FillGridView(
+                txtCategoryName.Text,
+                txtProductName.Text,
+                string.IsNullOrEmpty(txtFromPrice.Text) ? (int?) null : Convert.ToInt32 (txtFromPrice.Text),
+                string.IsNullOrEmpty(txtToPrice.Text) ? (int?) null : Convert.ToInt32 (txtToPrice.Text));
         }
         private void FillGridView(string categoryName, string productName, int? fromPrice, int? toPrice)
         {
-            using (var sqlConnection = new SqlConnection);
-            hhgj
+
+            string connectionString = ConfigurationManager.AppSettings["CsharpSampleDBConnectionString"];
+
+            using (
+                SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand();
+                command.Connection = sqlConnection;
+                command.CommandText = "sp_GetProducts";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@CategoryName", categoryName);
+                command.Parameters.AddWithValue("@ProductName", productName);
+                if (fromPrice != null)
+                    command.Parameters.AddWithValue("@fromprice", fromPrice);
+                if (toPrice != null)
+                    command.Parameters.AddWithValue("@toPrice", toPrice);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.SelectCommand = command;
+                DataTable dtProduct = new DataTable();
+                adapter.Fill(dtProduct);
+
+
+                dataGridView1.DataSource = dtProduct;
+            }
+            
         }
     }
 }
